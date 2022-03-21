c4300cba9e68f59ac1332ae56918ca6adc16de33
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-03-21 13:34:19 +0100

    WIP: tor controller in suite-desktop

--- packages/suite-desktop/src-electron/libs/processes/TorProcess.ts
+++ packages/suite-desktop/src-electron/libs/processes/TorProcess.ts
@@ -1,19 +1,22 @@
-import fetch from 'node-fetch';
-
 import BaseProcess, { Status } from './BaseProcess';
 
+import { TorController } from '@trezor/request-manager';
+
+// TODO: use DEFAULT_TOR_ADDRESS from request-manager intead of DEFAULT_ADDRESS
 // 9050 is the default port of the tor process.
 export const DEFAULT_ADDRESS = '127.0.0.1:9050';
 
 class TorProcess extends BaseProcess {
+    torController: TorController;
+
     constructor() {
         super('tor', 'tor');
+        this.torController = new TorController({ host: '127.0.0.1', port: 9050 });
     }
 
     async status(): Promise<Status> {
-        // service
         try {
-            const resp = await fetch(`http://${DEFAULT_ADDRESS}/`);
+            const resp = await this.torController.status();
             if (resp.status === 501 && resp.statusText.startsWith('Tor')) {
                 return {
                     service: true,
